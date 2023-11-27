e89b36f9480907819a6ce1fabb3c877aefdb9c64
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2023-11-27 13:42:09 +0100

    Start

--- Assets/Scripts/Events/EventsGameManager.cs
+++ Assets/Scripts/Events/EventsGameManager.cs
@@ -1,6 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EventsGameManager : MonoBehaviour
 {
+    public delegate void OnEnterKeyPressed();
+    public static OnEnterKeyPressed onEnterKeyPressed;
     // HERE YOU WILL HAVE TO DECLARE AND INVOKE THE EVENTS
+    public delegate void OnEscapeKeyPressed();
+    public static OnEscapeKeyPressed onEscapeKeyPressed;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            if (onEnterKeyPressed != null) {
+                onEnterKeyPressed();
+            }
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape)) {
+            if (onEscapeKeyPressed != null) {
+                onEscapeKeyPressed();
+            }
+            
+        }
+    }
 }
